392205cd4716c49b426a5e84866ecbc192f13bb6
Shailesh Vasandani <github@shaile.sh>
2020-08-09 00:20:25 -0400

    Add boilerplate http API code

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@ use s3::bucket::Bucket;
 use s3::region::Region;
 use s3::S3Error;
 use s3::creds::Credentials;
+use std::str;
+
+use warp::Filter;
+
+const MESSAGE: &str = "I want to go to S3";
 
 use std::env;
 
@@ -13,7 +18,18 @@ struct Storage {
     location_supported: bool,
 }
 
-fn main() -> Result<(), S3Error> {
+#[tokio::main]
+async fn main() {
+    // GET /hello/warp => 200 OK with body "Hello, warp!"
+    let hello = warp::path!("hello" / String)
+        .map(|name| format!("Hello, {}!", name));
+
+    warp::serve(hello)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
+}
+
+fn test() -> Result<(), S3Error> {
     // let bucket_name = &env::var("S3_BUCKET_NAME").unwrap();
     // let region = &env::var("AWS_REGION").unwrap();
     let aws = Storage {
@@ -34,8 +50,28 @@ fn main() -> Result<(), S3Error> {
     let results = bucket.list_blocking("".to_string(), None)?;
     for (list, code) in results {
         assert_eq!(200, code);
-        println!("{:?}", list.contents.len());
+        println!("{:?}", list.contents);
     }
+
+    let (_, code) =
+        bucket.put_object_blocking("test_file", MESSAGE.as_bytes(), "text/plain")?;
+    // println!("{}", bucket.presign_get("test_file", 604801)?);
+    assert_eq!(200, code);
+
+    // Get the "test_file" contents and make sure that the returned message
+    // matches what we sent.
+    let (data, code) = bucket.get_object_blocking("test_file")?;
+    let string = str::from_utf8(&data)?;
+    println!("{}", string);
+    assert_eq!(200, code);
+    assert_eq!(MESSAGE, string);
+
+    if aws.location_supported {
+        // Get bucket location
+        println!("{:?}", bucket.location_blocking()?);
+    }
+
+    let (_, code) = bucket.delete_object_blocking("test_file")?;
 
     Ok(())
 }
