6ad49137e54fc61f8665b97db0681dbb1a0361b9
Shailesh Vasandani <github@shaile.sh>
2020-08-08 23:34:24 -0400

    List items in S3 bucket

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use s3::bucket::Bucket;
 use s3::region::Region;
-use awscreds::Credentials;
+use s3::S3Error;
+use s3::creds::Credentials;
+
 use std::env;
 
 struct Storage {
@@ -11,22 +13,22 @@ struct Storage {
     location_supported: bool,
 }
 
-fn main() {
+fn main() -> Result<(), S3Error> {
     // let bucket_name = &env::var("S3_BUCKET_NAME").unwrap();
     // let region = &env::var("AWS_REGION").unwrap();
     let aws = Storage {
         name: "aws".into(),
-        region: "us-east-1".parse()?,
+        region: "us-east-2".parse()?,
         credentials: Credentials::from_env_specific(Some("AWS_ACCESS_KEY_ID"), Some("AWS_SECRET_ACCESS_KEY"), None, None,)?,
-        bucket: "rusty-kebab=bucket".to_string(),
-        location_support: true
+        bucket: "rusty-kebab-bucket".to_string(),
+        location_supported: true,
     };
 
     // let bucket = Bucket::new(bucket_name, region, credentials);
 
-    println!("Running {}", backend.name);
+    println!("Running {}", aws.name);
     // Create Bucket in REGION for BUCKET
-    let bucket = Bucket::new(&backend.bucket, backend.region, backend.credentials)?;
+    let bucket = Bucket::new(&aws.bucket, aws.region, aws.credentials)?;
 
     // List out contents of directory
     let results = bucket.list_blocking("".to_string(), None)?;
